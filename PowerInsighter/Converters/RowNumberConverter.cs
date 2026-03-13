79c887c35d9d8fa0be507ef5cf3c57c0bd9f6176
Akhil Grandhi <Akhilsaimanoj1@gmail.com>
2026-03-13 12:56:16 +0530

    SNO Added

--- PowerInsighter/Converters/RowNumberConverter.cs
+++ PowerInsighter/Converters/RowNumberConverter.cs
@@ -5,20 +5,26 @@ using System.Windows.Data;
 namespace PowerInsighter.Converters;
 
 /// <summary>
-/// Converter that returns the row number (1-based index) of a DataGridRow.
+/// Multi-value converter that calculates the 1-based row number from the DataGrid items.
 /// </summary>
-public class RowNumberConverter : IValueConverter
+public class RowNumberConverter : IMultiValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
-        if (value is DataGridRow row)
+        // values[0] = current item
+        // values[1] = ItemsSource collection
+        if (values.Length >= 2 && values[0] != null && values[1] is System.Collections.IList items)
         {
-            return row.GetIndex() + 1;
+            int index = items.IndexOf(values[0]);
+            if (index >= 0)
+            {
+                return (index + 1).ToString();
+            }
         }
-        return 0;
+        return "0";
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
     }
